fb7e6ef024f28df4aa6fe5b249f8924eed6b3b6d
Abhinav Pradeep <Abhinav.Pradeep@hotmail.com>
2020-02-09 18:39:05 +1000

    School commit
    SCONUG

---
+++ Heading.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Welcome to SCONUG.org';
+var speed = 100;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter()
